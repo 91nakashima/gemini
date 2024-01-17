91d6fd187fc1692ddf125eb1f35701111cfd18f7
91nakashima <91nakashima@gmail.com>
2024-01-17 10:15:51 +0900

    環境変数に設定

---
+++ key.py
@@ -0,0 +1,14 @@
+
+import os
+
+
+GOOGLE_CSE_ID = os.environ["GOOGLE_CSE_ID"]
+GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
+
+
+PROJECT_ID = os.environ["PROJECT_ID"]
+REGION = os.environ["REGION"]
+
+
+# Notion
+NOTION_API_KEY = os.environ["NOTION_API_KEY"]
