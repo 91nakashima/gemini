a617a208ac787e8599ff18fd3339a8ca90975f9e
91nakashima <91nakashima@gmail.com>
2024-01-17 10:20:54 +0900

    修正

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from gemini import GenimiAI
+import argparse
 
-g = GenimiAI()
-res = g.get_anything_chat('八日市の次回のゴミの日は？')
-print(res)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='GenimiAI')
+    parser.add_argument('-q', '--query', help='query', required=True)
+    args = parser.parse_args()
+    g = GenimiAI()
+    res = g.get_anything_chat(args.query)
+    print(res)
